d4e48357398c74e3d89ea09dfc14c23abf838ca5
Priyu-code14 <priyaa6688@gmail.com>
2026-07-02 09:12:17 +0530

    Initial commit

---
+++ basic.py
@@ -0,0 +1,78 @@
+#print("Hello World")
+#name=input()
+#print("Hello" + " " + name + ", " + "Welcome to Python!") or name=input("Enter Name:")
+#print(f"Hello {name}, Welcome to Python!")
+
+#l=int(input("Enter Length:"))
+#b=int(input("Enter Breadth"))
+#area=l*b
+#print("Area:",area)
+
+#a=10
+#b=5
+#temp=a
+#a=b
+#b=temp
+#print("a =",a)
+#print("b =",b)
+
+#a = 10
+#b = 20
+#a, b = b, a
+#print("a =", a)
+#print("b =", b)
+# a=int(input("Enter a:"))
+# b=a**2
+# c=a**3
+# print("Square:",b)
+# print("Cube:",c)
+
+# a=int(input("Enter a:"))
+# if (a%2==0):
+#     print("Even")
+# else:
+#     print("odd")
+
+# a=int(input("Enter a:"))
+# if (a>0):
+#     print("Positive")
+# elif (a<0):
+#     print("Negative")
+# else:
+#     print("Zero")
+
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# c=int(input("Enter c:"))
+# if (a>b and a>c):
+#     print("a is Largest")
+# elif(b>a and b>c):
+#     print("b is Largest")
+# else:
+#     print("c is Largest")
+
+# age=int(input("Enter age:"))
+# if (age>=18):
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# year=int(input("Enter Year:"))
+# if (year%4==0):
+#     print("Leap Year")
+# elif (year%400 == 0):
+#     print("Leap Year")
+# elif (year%100==0):
+#     print("Not leap year")
+# else:
+#     print("Not leap year")
+
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# c=int(input("Enter c:"))
+# if (a<=b and a<=c):
+#     print("a is Smallest")
+# elif(b<=a and b<=c):
+#     print("b is Smallest")
+# else:
+#     print("c is Smallest")
